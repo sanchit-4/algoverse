c8d47e462053b2365a57d5af60da555fbf1aa689
sanchit-4 <sanchitgoyal28032006@gmail.com>
2026-02-18 18:50:36 +0530

    next configs fixed for deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   async rewrites() {
     return [
       {
-        output: 'standalone',
         source: '/api/:path*', // When frontend hits /api/submit
         destination: 'http://localhost:3000/:path*', // Forward to Backend
       },
